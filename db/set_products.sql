e7df6171aa29a571972b02f35c3ea305d904d789
MeganMFisher <meganmichelle318@gmail.com>
2017-03-07 11:49:35 -0700

    set up database

---
+++ db/set_products.sql
@@ -0,0 +1,15 @@
+CREATE TABLE test
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(40)
+);
+
+INSERT INTO test (id, name) VALUES (1, 'test1');
+INSERT INTO test (id, name) VALUES (2, 'test2');
+INSERT INTO test (id, name) VALUES (3, 'test3');
+INSERT INTO test (id, name) VALUES (4, 'test4');
+INSERT INTO test (id, name) VALUES (5, 'test5');
+INSERT INTO test (id, name) VALUES (6, 'test6');
+INSERT INTO test (id, name) VALUES (7, 'test7');
+INSERT INTO test (id, name) VALUES (8, 'test8');
+INSERT INTO test (id, name) VALUES (9, 'test9');
