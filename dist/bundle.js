d364f7e007ae2b5c1816cda85d6ae2de24bd02e9
MeganMFisher <meganmichelle318@gmail.com>
2017-03-13 11:42:13 -0600

    added gulp

---
+++ dist/bundle.js
@@ -0,0 +1,66 @@
+'use strict';
+
+angular.module('app', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('home', {
+        url: '/',
+        templateUrl: '../views/home.html'
+
+    }).state('aboutus', {
+        url: '/aboutus',
+        templateUrl: '../views/aboutUs.html'
+
+    })
+
+    // .state('ourpromise', {
+    //         url: '/ourpromise',
+    //         parent: 'aboutus',
+    //         templateUrl: "../views/ourpromise.html"
+    //     })
+
+    // .state('process', {
+    //         url: '/process',
+    //         parent: 'aboutus',
+    //         templateUrl: "../views/process.html"
+    //     })
+
+    // .state('team', {
+    //         url: '/team',
+    //         parent: 'aboutus',
+    //         templateUrl: "../views/team.html"
+    //     })
+
+    .state('jointhefight', {
+        url: '/jointhefight',
+        templateUrl: '../views/joinTheFight.html'
+
+    }).state('ourstories', {
+        url: '/ourstories',
+        templateUrl: '../views/ourStories.html'
+
+    }).state('partnerships', {
+        url: '/partnerships',
+        templateUrl: '../views/partnerships.html'
+
+    }).state('eventsmedia', {
+        url: '/eventsmedia',
+        templateUrl: '../views/eventsMedia.html'
+
+    });
+});
+'use strict';
+
+angular.module('app').directive('navbar', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '../views/navbar.html'
+    };
+});
+'use strict';
+
+angular.module('app').controller('mainCtrl', function ($scope, mainSrv) {});
+'use strict';
+
+angular.module('app').service('mainSrv', function () {});
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const sass = require('gulp-sass');
+
+gulp.task('js', function() {
+  gulp.src(['./js/app.js', './js/**/*.js'])
+  .pipe(babel({
+    presets: ['es2015']
+  }))
+  .pipe(concat('bundle.js'))
+  .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('css', function() {
+  gulp.src([ 
+    './reset.css', 
+    './styles/**/*.css',
+    './styles/**/*.scss'
+    ])
+  .pipe(sass())
+  .pipe(concat('bundle.css'))
+  .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['js', 'css', 'watch']);
+
+gulp.task('watch', function(){
+  return gulp.watch(['./index.html', './js/**/*.js', './styles/**/*.css', './styles/**/*.scss'], ['js', 'css']);
+})
