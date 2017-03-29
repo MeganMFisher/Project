90d7e8e1f0162d7b388f1dce140f3c8526d36aa9
MeganMFisher <meganmichelle318@gmail.com>
2017-03-29 10:08:58 -0600

    fixing sizing

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -48,6 +48,9 @@ angular.module('app').directive('navbar', function () {
 });
 'use strict';
 
+angular.module('app').service('mainSrv', function () {});
+'use strict';
+
 angular.module('app').controller('mainCtrl', function ($scope, mainSrv) {
 
     $scope.showVideo = false;
@@ -166,6 +169,3 @@ angular.module('app').controller('mainCtrl', function ($scope, mainSrv) {
         });
     });
 });
-'use strict';
-
-angular.module('app').service('mainSrv', function () {});
