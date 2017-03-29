71439037e60359bdd6916676b68c2545ca4e1d63
MeganMFisher <meganmichelle318@gmail.com>
2017-03-28 20:26:23 -0600

    fixed measurements home

--- public/js/navDir.js
+++ public/js/navDir.js
@@ -1,6 +1,6 @@
 angular.module('app').directive('navbar', function() {
     return {
         restrict: 'E',
-        templateUrl: '../views/navbar.html',
+        templateUrl: './views/navbar.html',
     }
 })
